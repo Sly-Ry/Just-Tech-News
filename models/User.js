0876b4fc0a47d4eb7d978969b7be307b7ac04bea
ryan <ryan.williams93@aol.com>
2022-02-01 14:17:25 -0600

    created user db using sequelize/handled errors

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 
 // create our User Model
 class User extends Model {}
@@ -9,11 +9,11 @@ User.init(
     {
         // define an id column
         id: {
-            // use the special Sequelize Datatypes object provide
+            // use the special Sequelize DataTypes object provide what type of data it is
             type: DataTypes.INTEGER,
-            // this is equivalent of SQL's `NOT NULL` option
+            // this is the equivalent of SQL's `NOT NULL` option
             allowNull: false,
-            // intruct that this is the Primary Key
+            // instruct that this is the Primary Key
             primaryKey: true,
             // turn on auto increment
             autoIncrement: true
@@ -34,28 +34,21 @@ User.init(
                 isEmail: true
             }
         },
-        // define a passworf column
+        // define a password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                // this means the password must be at least four characters long 
-                len: [4]
+            // this means the password must be at least four characters long
+            len: [4]
             }
         }
     },
     {
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
-
-        // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
-        // don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // don't pluralize name of database table
         freezeTableName: true,
-        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
         underscored: true,
-        // make it so our model name stays lowercase in the database
         modelName: 'user'
     }
 );
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// File to collect the packaged API routes
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
