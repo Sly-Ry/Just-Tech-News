702e861fa6bf147277b2500d52b14550b93229e2
ryan <ryan.williams93@aol.com>
2022-02-08 10:59:26 -0600

    created homepage template

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ both cleartext and plaintext refer to unencrypted text.
 You’ll always need to define a model before you can create associations between that model and other models you’ve already created.
 
 The 404 status code identifies a user error and will need a different request for a successful response.
+
+LINK ADDRESS: https://handlebarsjs.com/guide/#html-escaping
+
+Three brackets will render the data as HTML.
+    Ex. The main.handlebars file used three curly brackets as {{{ body }}}.
+
+Two brackets will convert HTML characters to strings. For instance, "<" would become "&lt";
+    Ex. Two brackets as {{ title }}. 
+
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,17 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    // We're going to take a single "post" object and pass it to the homepage.handlebars template. Each property on the object (id, post_url, title, etc.) becomes available in the template using the Handlebars.js {{ }} syntax.
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
 });
-
 module.exports = router;
